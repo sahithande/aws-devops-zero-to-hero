d88ee05861b99fe41ef188ac41a5cc5dd7799749
sahithande <143463487+sahithande@users.noreply.github.com>
2024-02-02 21:25:42 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sahithande/sample-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sahithande/sample-python-flask-app:latest
+docker run -d -p 5000:5000 sahithande/sample-python-app:latest
