f8afb723a444b171a535523846111364a8e8d813
sahithande <143463487+sahithande@users.noreply.github.com>
2024-02-02 21:57:07 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,6 +4,6 @@ set -e
 
 docker pull sahithande/sample-python-app:latest
 
-container_id="docker ps | awk -F" " '{print $1}'"
+container_id='docker ps | awk -F" " '{print $1}''
 
 docker rm -f $container_id
