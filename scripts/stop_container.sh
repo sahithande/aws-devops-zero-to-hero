f23a576a21390ca159db0340ff90f7f7ed2c04c9
sahithande <143463487+sahithande@users.noreply.github.com>
2024-02-02 21:33:49 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+docker pull sahithande/sample-python-app:latest
+
+container_id="docker ps | awk -F" " '{print $1}'"
+
+docker rm -f $container_id
